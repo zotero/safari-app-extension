7bd85d1e0471cd66422d9b38becbed897b96096c
Michal Rentka <michalrentka@gmail.com>
2019-08-12 12:22:07 +0200

    #1 - Implemented http request proxy function for JS

--- SafariExtension/SafariExtensionHandler.swift
+++ SafariExtension/SafariExtensionHandler.swift
@@ -18,15 +18,64 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
 
     // MARK: - Actions
 
-    private func loadData(completed: @escaping (Int, String?) -> Void) {
-        let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
-
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let httpResponse = response as? HTTPURLResponse else { return }
-            let responseData = data.flatMap({ try? JSONDecoder().decode(ApiResponseData.self, from: $0) })
-            completed(httpResponse.statusCode, responseData?.title)
+    /// Performs a HTTP request from provided data.
+    /// - parameter data: Data for HTTP request. Data has to contain a valid url string and http method.
+    ///                   Data can contain additional options for the request, such as headers, body and timeout.
+    /// - parameter completion: Completion block of request.
+    /// - parameter statusCode: Response status code. Can be -1 if URL was not specified correctly
+    ///                         or -2 if Method was not specified.
+    /// - parameter url: URL of HTTP request.
+    /// - parameter response: String response from the request.
+    private func performHttpRequest(with data: [String: Any],
+                                    completion: @escaping (_ statusCode: Int, _ url: String, _ response: String?) -> Void) {
+        guard let urlString = data["url"] as? String,
+              let url = URL(string: urlString) else {
+            completion(-1, "missing/incorrect url", nil)
+            return
         }
 
+        guard let method = data["method"] as? String else {
+            completion(-2, urlString, nil)
+            return
+        }
+
+        let options = data["options"] as? [String: Any] ?? [:]
+        let headers = options["headers"] as? [String: Any]
+        let body = (options["body"] as? String).flatMap({ $0.data(using: .utf8) })
+        let timeout = (options["timeout"] as? Double) ?? 60
+
+        self.performHttpRequest(url: url, method: method, headers: headers,
+                                body: body, timeout: timeout, completion: completion)
+    }
+
+    /// Perform a HTTP request.
+    /// - parameter url: URL for the request.
+    /// - parameter method: HTTP method of the request.
+    /// - parameter headers: Optional HTTP headers for the request.
+    /// - parameter body: Optional body data for request.
+    /// - parameter timeout: Timeout interval of the request.
+    /// - parameter completion: Completion block of request.
+    /// - parameter statusCode: Response status code. Can be -1 if URL was not specified correctly
+    ///                         or -2 if Method was not specified.
+    /// - parameter url: URL of HTTP request.
+    /// - parameter response: String response from the request.
+    private func performHttpRequest(url: URL, method: String, headers: [String: Any]?,
+                                    body: Data?, timeout: TimeInterval,
+                                    completion: @escaping (_ statusCode: Int, _ url: String, _ response: String?) -> Void) {
+        var urlRequest = URLRequest(url: url, timeoutInterval: timeout)
+        urlRequest.httpMethod = method
+        if let headers = headers {
+            for data in headers {
+                urlRequest.setValue("\(data.value)", forHTTPHeaderField: data.key)
+            }
+        }
+        urlRequest.httpBody = body
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse else { return }
+            let strResponse = data.flatMap({ String(data: $0, encoding: .utf8) })
+            completion(httpResponse.statusCode, url.absoluteString, strResponse)
+        }
         task.resume()
     }
 
@@ -34,10 +83,13 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
         switch messageName {
-        case "loadData":
-            self.loadData { code, title in
-                page.dispatchMessageToScript(withName: "loadedData", userInfo: ["code": code,
-                                                                                "title": title ?? ""])
+        case "performHttpRequest":
+            if let data = userInfo {
+                self.performHttpRequest(with: data) { statusCode, url, strResponse in
+                    page.dispatchMessageToScript(withName: "httpResponse", userInfo: ["url": url,
+                                                                                      "statusCode": statusCode,
+                                                                                      "response": (strResponse ?? "")])
+                }
             }
         default: break
         }
