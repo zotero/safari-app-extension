zotero/safari-app-extension

commit f099493b9da8a4fc273f36db6c47ef41561502f6

Restore ability to inspect the background page

SafariExtension/GlobalPage.swift
@@ -21,6 +21,7 @@ class GlobalPage: NSObject {
 	
 	static var context: JSContext? = {
 		let context = JSContext()
+		context?.isInspectable = true
 		context?.exceptionHandler = onError
 
 		// Add setTimeout, setInterval, etc.

commit 9f68f4d741368558c1dc41f1dbaec1ac5dd42660

Fix breakage on macOS <13.3
Regression from f099493b9da

SafariExtension/GlobalPage.swift
@@ -21,7 +21,9 @@ class GlobalPage: NSObject {
 	
 	static var context: JSContext? = {
 		let context = JSContext()
-		context?.isInspectable = true
+		if #available(macOS 13.3, *) {
+			context?.isInspectable = true
+		}
 		context?.exceptionHandler = onError
 
 		// Add setTimeout, setInterval, etc.